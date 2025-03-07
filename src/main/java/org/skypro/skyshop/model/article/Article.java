9bc114d2790c992c308330b414e21a7f28881677
sergFibih <sergfibih@ya.ru>
2025-03-07 22:50:43 +1000

    домашка готова

--- src/main/java/org/skypro/skyshop/model/article/Article.java
+++ src/main/java/org/skypro/skyshop/model/article/Article.java
@@ -1,5 +1,6 @@
 package org.skypro.skyshop.model.article;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.skypro.skyshop.model.search.Searchable;
 
 import java.util.Objects;
@@ -22,11 +23,13 @@ public class Article implements Searchable {
                 + this.text;
     }
 
+    @JsonIgnore
     @Override
     public String getSearchTerm() {
         return this.toString();
     }
 
+    @JsonIgnore
     @Override
     public String getTypeObject() {
         return "ARTICLE";
---
+++ src/main/java/org/skypro/skyshop/service/SearchService.java
@@ -0,0 +1,30 @@
+package org.skypro.skyshop.service;
+
+import org.skypro.skyshop.model.product.Product;
+import org.skypro.skyshop.model.search.SearchResult;
+import org.skypro.skyshop.model.search.Searchable;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Service
+public class SearchService {
+    private final StorageService storageService;
+
+    public SearchService(StorageService storageService) {
+        this.storageService = storageService;
+    }
+
+
+
+    public Collection<SearchResult> search (String requestString){
+        Collection<SearchResult> results = storageService.getSearchables()
+                .stream()
+                .filter(searchable -> searchable.getSearchTerm().contains(requestString))
+                .map(SearchResult::fromSearchable)
+                .collect(Collectors.toList());
+        return results;
+    }
+}
